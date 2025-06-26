b76e7d6f1e61b5215c4c632187b073eb22a6bf91
Macbook Pro <macbookpro@Zakx-2.local>
2025-06-26 16:33:37 +0100

    Initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img className="globe" src="images/globe.png" alt="globe icon"/>
+            <span className="header-text">my travel journal.</span>
+        </header>
+    )
+}
